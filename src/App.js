22d13b33d7c5bae7c14e2f9f8340b167f4fa3453
JohannGab <johann.gabelle@outlook.fr>
2019-07-15 17:03:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import theBest from'./Images/meilleurMamanDuMonde.png';
+import comments from'./Images/comments.png';
+import './AppBluesquare.scss'
 
-function App() {
+import Change from'./Change.json';
+
+class App extends Component {
+  state = {
+    changeImage: true,
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="containerBase">
+    <div className="container">
+        <div className="shopContainer">
+          <img src={theBest} alt="" ></img>
+        </div>
+        <div>
+            <h5>Tote bag pour une<br></br> meilleure maman lt 3</h5>
+        </div>
+        <div>
+            <p className="pDeco">-</p>
+        </div>
+        <div>
+            <p>21,90 €</p>
+        </div>
+      </div>
+      <div className="container2">
+          <div>
+            <img src={comments} alt=""></img>
+            <img src={comments} alt=""></img>
+            <img src={comments} alt=""></img>
+          </div>
+      </div>
+      <div className="container3">
+          <div>
+            <h3>ACHETER <span>|</span>CRÉER</h3>
+          </div>
+      </div>
+      <div className="container4">
+          <div>
+            <p><img src="" alt=""></img>Bouriche-Hamza</p>
+          </div>
+      </div>
+      </div>
+
+    </>
+  
   );
 }
+}
+
 
 export default App;

2b445618aa147a7228b931c5b66479a612ff130e
JohannGab <johann.gabelle@outlook.fr>
2019-07-19 16:35:23 +0200

    after test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import theBest from'./Images/meilleurMamanDuMonde.png';
-import comments from'./Images/comments.png';
+// import comments from'./Images/comments.png';
 import './AppBluesquare.scss'
 
 import Change from'./Change.json';
 
 class App extends Component {
   state = {
-    changeImage: true,
+    images: theBest,
+    changeImageClik: Change,
+    i: 0,
   }
 
+
+
+  changeImage = () => {
+    const size = Change
+      if (this.state.i < size.length){
+      const url = this.state.changeImageClik[this.state.i]
+      const imageUrl = url.changeImages
+      this.setState({ images: imageUrl })
+      this.setState({ i: this.state.i+1 })
+      console.log(this.state.i);
+  } else {
+    
+  }
+}
+
+  
   render() {
-  return (
+        
+    return (
     <>
     <div className="containerBase">
     <div className="container">
         <div className="shopContainer">
-          <img src={theBest} alt="" ></img>
+          <img onClick={this.changeImage} src={this.state.images} alt="" ></img>
         </div>
         <div>
             <h5>Tote bag pour une<br></br> meilleure maman lt 3</h5>
@@ -30,19 +49,19 @@ class App extends Component {
       </div>
       <div className="container2">
           <div>
-            <img src={comments} alt=""></img>
-            <img src={comments} alt=""></img>
-            <img src={comments} alt=""></img>
+            <i className="fas fa-heart"></i>
+            <i className="fas fa-comment-alt"></i>
+            <i className="fas fa-share-square"></i>
           </div>
       </div>
       <div className="container3">
           <div>
-            <h3>ACHETER <span>|</span>CRÉER</h3>
+            <h3>ACHETER <span>|</span> CRÉER</h3>
           </div>
       </div>
       <div className="container4">
           <div>
-            <p><img src="" alt=""></img>Bouriche-Hamza</p>
+            <p><i className="fas fa-user"></i>Bouriche-Hamza</p>
           </div>
       </div>
       </div>
